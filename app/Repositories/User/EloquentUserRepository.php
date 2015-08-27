7a0e668396885d423b28a697794745dfb07b4ff6
Joe Alamo <joe.keilty+github@gmail.com>
2015-08-27 19:27:42 +0100

    Override create method in EloquentUserRepository
    Take the nickname and youtube_id and create a new User entity.

--- app/Repositories/User/EloquentUserRepository.php
+++ app/Repositories/User/EloquentUserRepository.php
@@ -10,6 +10,7 @@ namespace Playlister\Repositories\User;
 
 
 use Playlister\Models\User;
+use Laravel\Socialite\Contracts\User as YoutubeUser;
 use Playlister\Repositories\EloquentRepository;
 
 class EloquentUserRepository extends EloquentRepository implements UserRepository
@@ -24,6 +25,16 @@ class EloquentUserRepository extends EloquentRepository implements UserRepositor
         $this->model = $model;
     }
 
+    public function create(YoutubeUser $youtubeUser)
+    {
+        $user = new User();
+        $user->name = $youtubeUser->getNickname();
+        $user->youtube_id = $youtubeUser->getId();
+        $user->save();
+
+        return $user;
+    }
+
     /**
      * @desc Return whether user exists tied to $youtubeId
      * @param $youtubeId
@@ -33,4 +44,4 @@ class EloquentUserRepository extends EloquentRepository implements UserRepositor
     {
         return $this->model->where('youtube_id', $youtubeId)->count() > 0;
     }
-} 
+}
